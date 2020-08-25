fba9065676529aa54e9453a371e05608a3390aed
Supiraru <farhandewanta11@gmail.com>
2020-08-25 22:37:27 +0700

    Added Verification code to login, (HARD)

--- App.py
+++ App.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from dictionary import dictionary
 import mysql.connector
+from smtplib import SMTP
+from random import randint
+
 
 root = Flask(__name__,static_url_path='/static')
 root.config['DB_USER'] = 'Aan'
@@ -8,7 +11,20 @@ root.config['DB_PASSWORD'] = 'Abc123de45'
 root.config['DB_NAME'] = 'testdata'
 root.config['DB_HOST'] = 'localhost'
 
-conn = cursor = None
+conn = cursor = RealName = VerifCode = None
+
+def RandCode():
+    VerificationCode = randint(1000,10000)
+    message = "Are you trying to login? This is your verification code for Aan Website : \n" + str(VerificationCode) + "\n BEWARE, DONT TELL ANYONE THIS VERIFICATION CODE"
+    return VerificationCode, message
+
+def sendMail(receiver, VerifMessage):
+    server = SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login("Supiraru11@gmail.com", "Programmer1234")
+    server.sendmail("Supiraru11@gmail.com", receiver, VerifMessage)
+    server.quit()
 
 def openDb():
     global conn, cursor
@@ -73,15 +89,18 @@ def dictFunc():
 
 @root.route('/login',  methods = ['GET', 'POST'])
 def login():
+    global RealName, VerifCode
     if request.method == "POST":
         UsernameInput = request.form['Username']
         openDb()
         cursor.execute("SELECT * FROM Account")
         for x in cursor:
             if x[1] == UsernameInput:
-                Name = x[2]
+                RealName = x[2]
+                VerifCode, message = RandCode()
+                sendMail(x[3], message)
                 closeDb()
-                return render_template('SuccesfulLogin.html', Name = Name)
+                return render_template('Verification.html')
         return render_template('Login.html', info = "Wrong Username")
     return render_template('Login.html')
 
@@ -97,7 +116,16 @@ def register():
         closeDb()
         return render_template('home.html')
     return render_template('register.html')
-    
+
+@root.route('/verification', methods = ['GET', 'POST'])
+def verification():
+    if request.method == "POST":
+        verifInput = request.form['Verification']
+        if verifInput == str(VerifCode):
+            print("DONE")
+            return render_template('SuccesfulLogin.html', Name = RealName)
+        return render_template('Verification.html', info = "Wrong Verification code")
+    return render_template('Verification.html')
 
 if __name__ == '__main__':
     root.run(debug = True)
